28ecbc5157c4d3d1a31ca6866aceeb3f2861d012
Sam Goldsmith <samggold@gmail.com>
2020-07-03 12:41:58 -0600

    Popup with submit button to append field

--- content.js
+++ content.js
@@ -1,6 +1,18 @@
+function buildMarkup() {
+  var checkedBoxes = document.querySelectorAll('input[name="markup"]:checked');
+  var markup = '';
+
+  for (var i = 0; i < checkedBoxes.length; i++) {
+    markup += markupMap[checkedBoxes[i].id] + '\n';
+  }
+  
+  console.log(markup)
+  return markup;
+}
+
 function onSubmit(event) {
-  console.log('Submitted!' + event.timeStamp);
   event.preventDefault();
+  buildMarkup();
 }
 
 document.addEventListener('DOMContentLoaded', function() {
@@ -8,3 +20,12 @@ document.addEventListener('DOMContentLoaded', function() {
 
   form.addEventListener('submit', onSubmit);
 });
+
+var markupMap = {
+  whatDoesItDo: '### What does it do?',
+  whatElse: '### What else do you need to know?',
+  relatedTickets: '### Related Tickets',
+  testCoverage: '### Test Coverage',
+  communication: '### Communication',
+  screenShots: '### Screenshots'
+};
