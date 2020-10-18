ddeae56b25b69ee149fac26036adbb648ddbc43f
Sam <girlsam@hey.com>
2020-10-18 11:47:51 -0600

    Contrived change for testing

--- popup.js
+++ popup.js
@@ -1,42 +1,46 @@
 function buildMarkdown() {
-  var checkedBoxes = document.querySelectorAll('input[name="markdown"]:checked');
-  var markdown = '';
-  
+  var checkedBoxes = document.querySelectorAll(
+    'input[name="markdown"]:checked'
+  );
+  var markdown = "";
+
   for (var i = 0; i < checkedBoxes.length; i++) {
-    markdown += markdownMap[checkedBoxes[i].id] + '\n \n';
+    markdown += markdownMap[checkedBoxes[i].id] + "\n \n";
   }
-  
+
   return markdown;
 }
 
 function sendMessage() {
-  chrome.tabs.query({ active: true }, function(tabs) {
+  chrome.tabs.query({ active: true }, function (tabs) {
     return chrome.tabs.sendMessage(
-      tabs[0].id, // id of current tab
+      tabs[0].id, // current tab
       {
-        from: 'popup',
-        subject: 'addMarkdown',
-        value: buildMarkdown()
-      });
+        from: "popup",
+        subject: "addMarkdown",
+        value: buildMarkdown(),
+      }
+    );
   });
 }
 
 function onSubmit(event) {
   event.preventDefault();
   sendMessage();
+
   window.close();
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  var form = document.getElementById('form');
-  form.addEventListener('submit', onSubmit);
+document.addEventListener("DOMContentLoaded", function () {
+  var form = document.getElementById("form");
+  form.addEventListener("submit", onSubmit);
 });
 
 var markdownMap = {
-  whatDoesItDo: '### What does it do?',
-  whatElse: '### What else do you need to know?',
-  relatedTickets: '### Related Tickets',
-  testCoverage: '### Test Coverage',
-  communication: '### Communication',
-  screenShots: '### Screenshots'
+  whatDoesItDo: "### What does it do?",
+  whatElse: "### What else do you need to know?",
+  relatedTickets: "### Related Tickets",
+  testCoverage: "### Test Coverage",
+  communication: "### Communication",
+  screenShots: "### Screenshots",
 };
